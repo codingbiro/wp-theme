aa8b19691698073889e841828a34a4cf647801a6
codingbiro <quick.biro@gmail.com>
2019-03-08 21:38:15 +0100

    first days theme

--- demo/script.js
+++
@@ -1,8 +0,0 @@
-function scrolljs(elem) {
-    var a = document.getElementsByClassName('scrollable');
-    
-    for (i = 0; i < a.length; i++) {
-        a[i].classList.remove('active')
-    }
-    elem.classList.add('active');
-}
